813a4487917bfb6f57c9bb8a1c9b60be0daeed43
Leidy <hellocar907@gmail.com>
2023-04-24 20:04:12 -0500

    prueba de versionamiento en git

---
+++ Ejemplo2.java
@@ -0,0 +1,10 @@
+/**
+ * @author Leidy Cruz
+ */
+public class Ejemplo2 {
+    public static void main(String[] args) {
+        String nombre = "Dina Luz ";
+        nombre += "Ortega";
+        System.out.println(nombre);
+    }
+}
